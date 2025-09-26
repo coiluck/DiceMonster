543f5b0a8ca32e08164f5d0c04642d89f27cccf4
coiluck <wvy.wingle@gmail.com>
2025-09-26 09:50:14 +0900

    Add result Ui

--- js/module/result.js
+++ js/module/result.js
@@ -1,8 +1,17 @@
 // result.js
+import { globalGameState } from "./gameState.js";
+import { message } from "./message.js";
+import { changeModal } from "./changeModal.js";
+
 export function gameOver() {
   console.log('gameOver');
 }
 
 export function roundEnd() {
-  console.log('roundEnd');
+  changeModal('result', null, 500, false);
+  setTimeout(() => {
+    document.getElementById('result-header').classList.add('active');
+    document.querySelector('#result-header .result-header-round').classList.add('active');
+    document.getElementById('result-content').classList.add('active');
+  }, 500);
 }
